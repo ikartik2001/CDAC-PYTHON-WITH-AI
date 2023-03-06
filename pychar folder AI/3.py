21c07bf312750e4121215a24077d96cdf505087f
ikartik2001 <ikartikthakur@gmail.com>
2023-03-06 12:28:50 +0530

    created AI folder
    created a clearn folder in gitdesktop in cdac center for 3 months training .
    Advanced AI with python

---
+++ pychar folder AI/3.py
@@ -0,0 +1,12 @@
+fruits=["apple","mango","grapes","milk","bananas","oragne"]
+
+prices=[13,45,23,45,23,223]
+
+fruits_prices=zip(fruits,prices)
+print(fruits_prices)
+a=35
+b=a%2
+print(b)
+if (b)==0:
+    print("sdf")
+prices.inser
---
+++ pychar folder AI/chomu.py
@@ -0,0 +1,39 @@
+import random
+
+q1 = [1,2,3,4,5,6]
+q2=[7,8,9,10,11]
+
+#first operation : adding two list
+
+q3 = q1+q2
+print(q3)
+
+#2nd operation
+
+q1.append(12)
+print(q1)
+
+#3d operation
+
+test_list = ["kartik","singh","THAKUR"]
+newlist = test_list.copy()
+print(newlist)
+test_list.append("harmul")
+print(test_list)
+
+# vowels
+vowels = ["u","e","i","o","a"]
+num = [1,2,3,6,2,734,0,7]
+
+rubbish = ["kartik","THAKUR","SINGH","SUNDARNAGAR"]
+rubbish.sort()
+vowels.sort()
+num.sort()
+print(vowels)
+print(num)
+print(rubbish)
+
+
+
+
+#4th operation
