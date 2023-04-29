adcf941af932bd3fb177a2fc865574a68fa40048
kartik thakur <126507989+ikartik2001@users.noreply.github.com>
2023-04-29 13:40:11 +0530

    Delete OTHER FILES directory

--- OTHER FILES/other.py
+++
@@ -1,4 +0,0 @@
-d={"Name":['Rahul','Archana','Garish','Anjali','Anmol','Monika'],
-   "Subjects":['Python','c++','java','php','ruby','javascript'],
-   "Marks":[70,77,75,67,85,90],
-   "Higest Marks":[34,67,98,23,90,99]}
